b389087dbbac57e4cb8745d3ac0b413fedeeae9c
Rizkiputraramadhan <rizkikuliit@gmail.com>
2024-03-31 10:29:37 +0700

    refactoring accumulation

--- resources/views/v_akumulasi/index.blade.php
+++ resources/views/v_akumulasi/index.blade.php
@@ -27,7 +27,73 @@
     <p>None total : {{ $gender['none']['count'] }} = {{ $gender['none']['persen'] }} % dari keseluruhan data
         = {{ $sum }}</p>
 
+    <div style="display: flex; margin: 5px">
+        <table border="2">
+            <tr>
+                <th>Name</th>
+                <th>Gender</th>
+            </tr>
+            @foreach ($gender['male']['datas'] as $row)
+                <tr>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->gender }}</td>
+                </tr>
+            @endforeach
+            <tr>
+                <th>Jumlah</th>
+                <td>{{ $gender['male']['count'] }}</td>
+            </tr>
+            <tr>
+                <th>persentasi</th>
+                <td>{{ $gender['male']['persen'] }} %</td>
+            </tr>
+        </table>
+        <br>
+        <table border="2" style="margin-left: 5px; margin-right: 5px">
+            <tr>
+                <th>Name</th>
+                <th>Gender</th>
+            </tr>
+            @foreach ($gender['female']['datas'] as $row)
+                <tr>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->gender }}</td>
+                </tr>
+            @endforeach
+            <tr>
+                <th>Jumlah</th>
+                <td>{{ $gender['female']['count'] }}</td>
+            </tr>
+            <tr>
+                <th>persentasi</th>
+                <td>{{ $gender['female']['persen'] }} %</td>
+            </tr>
+        </table>
+        <br>
+        <table border="2">
+            <tr>
+                <th>Name</th>
+                <th>Gender</th>
+            </tr>
+            @foreach ($gender['none']['datas'] as $row)
+                <tr>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->gender }}</td>
+                </tr>
+            @endforeach
+            <tr>
+                <th>Jumlah</th>
+                <td>{{ $gender['none']['count'] }}</td>
+            </tr>
+            <tr>
+                <th>persentasi</th>
+                <td>{{ $gender['none']['persen'] }} %</td>
+            </tr>
+        </table>
+    </div>
+
 
 
 </body>
+
 </html>
