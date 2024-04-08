72ad9565465c1f45cf80a6138043e389748f92db
Rizkiputraramadhan <rizkikuliit@gmail.com>
2024-04-08 18:45:55 +0700

    tables and count beranda

--- resources/views/v_beranda/index.blade.php
+++ resources/views/v_beranda/index.blade.php
@@ -1,55 +1,86 @@
 @extends('layouts.app')
-@section('content')
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.6/css/jquery.dataTables.css">
 
+@section('content')
     <div class="flex flex-col w-full px-3 mt-8 space-x-0 space-y-3 md:space-y-0 md:space-x-2 md:flex-row h-28">
-        <div class="w-full h-full rounded-lg shadow-inner md:w-1/2 bg-gradient-red"></div>
-        <div class="w-full h-full rounded-lg shadow-inner md:w-1/2 bg-gradient-blue"></div>
+        <div class="w-full h-full rounded-lg shadow-inner md:w-1/2 bg-gradient-red">
+            <div class="flex">
+                <div class="p-5">
+                    <span class="text-white font-bold">Total Admin</span>
+                    <br>
+                    <p class="text-white font-extrabold mt-2">{{ $count_admin }}</p>
+                </div>
+                <div class="font-extrabold"></div>
+            </div>
+        </div>
+        <div class="w-full h-full rounded-lg shadow-inner md:w-1/2 bg-gradient-blue">
+            <div class="flex">
+                <div class="p-5">
+                    <span class="text-white font-bold">Total Pengunjung</span>
+                    <br>
+                    <p class="text-white font-extrabold mt-2">{{ $count_guest }}</p>
+                </div>
+                <div class="font-extrabold"></div>
+            </div>
+        </div>
     </div>
 
-    <table id="example" class="" style="width:100%">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
-            </tr>
-            <!-- More rows can be added here -->
-        </tbody>
-    </table>
+    <div class="bg-white m-2 rounded p-2">
+        <div class="mb-4">
+            <input type="text" id="searchInput" class="px-4 py-2 border rounded-md" placeholder="Search...">
+        </div>
 
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg ">
+            <table id="dataTable" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">#</th>
+                        <th scope="col" class="px-6 py-3">No. ID</th>
+                        <th scope="col" class="px-6 py-3">Nama</th>
+                        <th scope="col" class="px-6 py-3">Alamat</th>
+                        <th scope="col" class="px-6 py-3">Kota</th>
+                        <th scope="col" class="px-6 py-3">Tgl Lahir</th>
+                        <th scope="col" class="px-6 py-3">Pekerjaan</th>
+                        <th scope="col" class="px-6 py-3">Pendidikan</th>
+                        <th scope="col" class="px-6 py-3">Gender</th>
+                        <th scope="col" class="px-6 py-3">Jenis Tamu</th>
+                        <th scope="col" class="px-6 py-3">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data_filter as $index => $row)
+                        <tr
+                            class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $index + 1 }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->ID_identity }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->name }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->address }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->region }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->birth_date }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->work }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->education }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->gender }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $row->type_guest }}</td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <a href="#"
+                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
 
-    {{-- <form action="" method="post">
-        @csrf
-        <input type="date" name="start_date" value="{{ $startDate }}">
-        <input type="date" name="end_date" value="{{ $endDate }}">
-        <button type="submit">filter</button>
-    </form>
-    <a href="/beranda"><button>Reset</button></button>
-
-
-
-        <p>{{ $data_filter_count }}</p> --}}
-           
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <br>
+        {{ $data_filter->links() }}
+    </div>
 @endsection
